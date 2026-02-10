d07119a0a88a0e183aa0a5b32b7a775d6be2102b
OriNadjar <orimygame@gmail.com>
2026-02-10 12:43:01 +0200

    CR #1 fix

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,44 +1,63 @@
-import { useOutletContext } from 'react-router-dom';
+import { useOutletContext } from "react-router-dom";
 
-import ControlPanel from '../components/ControlPanel';
-import TodoList from '../components/TodoList';
-import TodoDialog from '../components/TodoDialog';
-import { Box } from '@mui/material';
-import MapComponent from '../components/MapComponent';
+import ControlPanel from "../components/ControlPanel";
+import TodoList from "../components/TodoList";
+import TodoDialog from "../components/TodoDialog";
+import { Box } from "@mui/material";
+import MapComponent from "../components/MapComponent";
 
-import { useTodos } from '../context/todoContext';
-
-interface AdminOutletContext {
-  openEditDialog: (id: string) => void;
-  handleOpenDialog: () => void;
-  isDialogOpen: boolean;
-  editingTodoId: string | null;
-  handleCloseDialog: () => void;
-}
+import { useTodos } from "../context/todoContext";
+import type { OutletContext } from "../types/types";
 
 const UserScreen = () => {
-    const { openEditDialog, handleOpenDialog, isDialogOpen, editingTodoId, handleCloseDialog } = useOutletContext<AdminOutletContext>();
-    const { filteredTodos } = useTodos();
+  const {
+    openEditDialog,
+    handleOpenDialog,
+    isDialogOpen,
+    editingTodoId,
+    handleCloseDialog,
+  } = useOutletContext<OutletContext>();
+  const { filteredTodos } = useTodos();
 
-    return (
-      <>
-        <Box sx={{ height: '100vh', maxHeight: '100vh',  display: 'flex' }}>
+  return (
+    <>
+      <Box sx={{ height: "100vh", maxHeight: "100vh", display: "flex" }}>
+        <Box
+          sx={{
+            flex: 1,
+            mr: 1,
+            border: "1px solid #ddd",
+            borderRadius: "8px",
+            overflowY: "auto",
+            "&::-webkit-scrollbar": { width: "0px" },
+          }}
+        >
+          <ControlPanel handleOpenDialog={handleOpenDialog} />
 
-          <Box sx={{ flex: 1, mr: 1, border: '1px solid #ddd', borderRadius: '8px', overflowY: 'auto', '&::-webkit-scrollbar': { width: '0px' } }}>
-            <ControlPanel  handleOpenDialog={handleOpenDialog}/>
+          <TodoList openEditDialog={openEditDialog} />
 
-            <TodoList openEditDialog={openEditDialog}/>
-
-            <TodoDialog handleCloseDialog={handleCloseDialog} isDialogOpen={isDialogOpen} editingTodoId={editingTodoId} />
-          </Box>
-
-          <Box sx={{ flex: 1, border: '1px solid #ddd', borderRadius: '8px', overflow: 'hidden', height: '100%', position: 'relative' }}>
-            <MapComponent todos={filteredTodos}/>
-          </Box>
-
+          <TodoDialog
+            handleCloseDialog={handleCloseDialog}
+            isDialogOpen={isDialogOpen}
+            editingTodoId={editingTodoId}
+          />
         </Box>
-      </>
-    )
-}
 
-export default UserScreen
+        <Box
+          sx={{
+            flex: 1,
+            border: "1px solid #ddd",
+            borderRadius: "8px",
+            overflow: "hidden",
+            height: "100%",
+            position: "relative",
+          }}
+        >
+          <MapComponent todos={filteredTodos} />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default UserScreen;
